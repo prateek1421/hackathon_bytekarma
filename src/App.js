51bf0e77f6e4e66a7afb7de7eeb71f7d692f8682
maheshsdehmukh <mahesh.s.deshmukh@gmail.com>
2019-10-10 03:04:26 +0530

    moved user object to app

--- src/App.js
+++ src/App.js
@@ -9,16 +9,32 @@ import Trade from "./component/trade";
 import Footer from "./component/footer";
 import axios from "axios";
 axios.defaults.baseURL="https://carma-function.azurewebsites.net";
-function App() {
-  return (
-    <React.Fragment>
-      <Navigaion />
-      <Header />
-      <Trade />
-      <AddExpenses />
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  state={
+    userObject:null
+  }
+  componentDidMount=()=>{
+    let urlDataArray = document.location.href.split("/");
+    console.log("urlDataArray");
+    axios.get("/api/dashoboard/"+urlDataArray[urlDataArray.length-1])
+    .then(json=>{
+      console.log("json.data");
+      console.log(json.data);
+    this.setState({userObject:json.data[0]});
+    }).catch(function(error){console.log(error);});
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <Navigaion />
+        <Header user={this.state.userObject}/>
+        <Trade />
+        <AddExpenses user={this.state.userObject}/>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+  
 }
 
 export default App;
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,24 +1,6 @@
 import React,{Component} from "react";
-import axios from "axios";
 class Header extends Component {
-  state ={
-    carbonPointsUsed:400,
-    totalCarbonPoints:1200,
-    percent:40
-  }
-  componentDidMount=()=>{
-    let urlDataArray = document.location.href.split("/");
-    console.log("urlDataArray");
-    axios.get("/api/dashoboard/"+urlDataArray[urlDataArray.length-1])
-    .then(json=>{
-      console.log("json.data"); 
-      console.log(json.data);
-    this.setState({carbonPointsUsed: json.data[0].CarbonPointsUsed,
-      totalCarbonPoints: json.data[0].TotalCarbonPoints,
-      percent:(json.data[0].CarbonPointsUsed*100/json.data[0].TotalCarbonPoints).toFixed(2)
-    })
-    }).catch(function(error){console.log(error);});
-  }
+   
   render(){
     return (
       <header className="header">
@@ -36,8 +18,12 @@ class Header extends Component {
             <div>
                 
               <div className="heading-primary--note">
-                carbon credits used:{this.state.percent}%
-                <div className="heading-primary--note__progress" style={{width:this.state.percent+"%"}}>&nbsp; </div>
+                carbon credits used:{this.props.user?(this.props.user.CarbonPointsUsed*100
+                /this.props.user.TotalCarbonPoints)
+        .toFixed(2):0}%
+                <div className="heading-primary--note__progress" style={{width:(this.props.user?(this.props.user.CarbonPointsUsed*100
+                /this.props.user.TotalCarbonPoints)
+        .toFixed(2):0)+"%"}}>&nbsp; </div>
               </div>
             </div>
           </h1>
